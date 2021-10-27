d442ec7f2af886236fc9cdb9d69e14d9e392d72f
ramonamorim <amorimramon@outlook.com>
2021-10-27 04:53:55 -0300

    Atualização - alterado metodo construtor da classe AccountInvalidDocumentExcpt.

--- src/main/java/com/pismo/exception/AccountInvalidDocumentExcpt.java
+++ src/main/java/com/pismo/exception/AccountInvalidDocumentExcpt.java
@@ -1,7 +1,5 @@
 package com.pismo.exception;
 
-import org.springframework.http.HttpStatus;
-
 public class AccountInvalidDocumentExcpt extends GenericException {
 
 	/**
@@ -9,10 +7,8 @@ public class AccountInvalidDocumentExcpt extends GenericException {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public static final String MESSAGE = "Documento informado %d é inválido.";
-
-	public AccountInvalidDocumentExcpt(String documentNumber) {
-		super(HttpStatus.NOT_FOUND, String.format(MESSAGE, documentNumber), documentNumber);
+	public AccountInvalidDocumentExcpt(String message) {
+		super(message);
 	}
 
 }
